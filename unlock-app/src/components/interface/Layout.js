af99996797d71c95bde32661325d7a4bbf3b1ffb
Ben Werdmuller <ben@benwerd.com>
2018-12-14 14:46:29 -0800

    Reformatted

--- unlock-app/src/components/interface/Layout.js
+++ unlock-app/src/components/interface/Layout.js
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import Header from './Header'
 import Footer from './Footer'
 import { RoundedLogo } from './Logo'
+import Media from '../../theme/Media'
 
 export default function Layout({ forContent, title, children }) {
   return (
@@ -43,10 +44,6 @@ Layout.defaultProps = {
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr minmax(280px, 4fr) 1fr;
-
-  ${Media.phone`
-    display: flex;
-  `};
 `
 
 const Left = styled.div`
@@ -54,19 +51,15 @@ const Left = styled.div`
   align-items: start;
   height: 24px;
 
-  & > * {
-    @media (max-width: 500px) {
-      display: none;
-    }
-  }
+  ${Media.phone`
+    display: none;
+  `};
 `
 
 const Right = styled.div`
-  & > * {
-    @media (max-width: 500px) {
-      display: none;
-    }
-  }
+  ${Media.phone`
+    display: none;
+  `};
 `
 
 const Content = styled.div`
